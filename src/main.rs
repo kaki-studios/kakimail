baf0e1e2f58a6512d756ceef5b930952a8361847
kaki-studios <kkirvela@gmail.com>
2024-01-22 22:14:02 +0200

    research(chatgpt), and tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::*;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    tracing_subscriber::fmt::init();
+    //NB cant use 25 since its blocked by isp/os???
+    let addr = "127.0.0.1:2525";
+
+    let listener = TcpListener::bind(&addr).await?;
+    tracing::info!("listening on: {}", addr);
+    loop {
+        let (_stream, addr) = listener.accept().await?;
+        tracing::info!("recieved new connection from {:?}", addr);
+    }
 }
